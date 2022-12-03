0050a7f1f4adfd9f7a0d853bdf8174929f4c933f
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-12-03 16:10:44 +0530

    Remove extra screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Deposit from "./pages/Deposit";
-import Explorer from "./pages/Explorer";
 import HomePage from "./pages/HomePage";
-import Minting from "./pages/Minting";
-import Settlement from "./pages/Settlement";
-import Transfer from "./pages/Transfer";
-import Withdrawal from "./pages/Withdrawal";
 import { Web3AuthProvider } from "./services/web3auth";
 
 function App() {
@@ -19,12 +13,6 @@ function App() {
           <Routes>
             <Route path="/">
               <Route index element={<HomePage />} />
-              <Route path="deposit" element={<Deposit />} />
-              <Route path="withdrawal" element={<Withdrawal />} />
-              <Route path="minting" element={<Minting />} />
-              <Route path="transfer" element={<Transfer />} />
-              <Route path="settlement" element={<Settlement />} />
-              <Route path="explorer" element={<Explorer />} />
             </Route>
           </Routes>
         </BrowserRouter>
